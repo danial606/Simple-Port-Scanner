18c5a17f73a02af97cf4935b6f9cd90d8fb9930f
Danial Abdullah <danialabdullah30@gmail.com>
2024-09-19 23:15:36 +0530

    Update portscan.py
    Added threading and timeout

--- portscan.py
+++ portscan.py
@@ -1,28 +1,28 @@
 import socket
 import termcolor
-
-
-def scan(target, ports):
-	print('\n' + ' Starting Scan For ' + str(target))
-	for port in range(1,ports):
-		scan_port(target,port)
-
+import threading
 
 def scan_port(ipaddress, port):
-	try:
-		sock = socket.socket()
-		sock.connect((ipaddress, port))
-		print("Port Opened " + str(port))
-		sock.close()
-	except:
-		pass
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print(termcolor.colored(("Port Opened " + str(port)), 'green'))
+        sock.close()
+    except:
+        pass
 
+def scan(target, ports):
+    print('\n' + ' Starting Scan For ' + str(target))
+    for port in range(1, ports + 1):
+        thread = threading.Thread(target=scan_port, args=(target, port))
+        thread.start()
 
-targets = input("Enter IPs To Scan ")
-ports = int(input("Enter How Many Ports You Want To Scan "))
+targets = input("Enter IPs To Scan (comma-separated for multiple): ")
+ports = int(input("Enter How Many Ports You Want To Scan: "))
 if ',' in targets:
-	print(termcolor.colored(("Scanning Multiple IPs"), 'green'))
-	for ip_addr in targets.split(','):
-		scan(ip_addr.strip(' '), ports)
+    print(termcolor.colored(("Scanning Multiple IPs"), 'green'))
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '), ports)
 else:
-	scan(targets,ports)
+    scan(targets, ports)
